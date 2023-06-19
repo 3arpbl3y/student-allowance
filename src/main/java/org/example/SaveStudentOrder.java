2d7c2fcfe6469bf9ba9678a244f6643bdeffc16c
Emelyanov Maxim <3arpbl3y@gmail.com>
2023-06-19 13:00:09 +0500

    add SaveStudentOrder

---
+++ src/main/java/org/example/SaveStudentOrder.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class SaveStudentOrder {
+    public static void main(String[] args) {
+        saveStudentOrder();
+    }
+
+    static void saveStudentOrder () {
+        System.out.println("new SSO");
+
+    }
+}
